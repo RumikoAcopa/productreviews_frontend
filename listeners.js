ca53a47cc88036ce2e7b46b0f07fa78839410148
rumikoacopa <rumikorios@gmail.com>
2021-01-12 00:35:57 -0500

    trying to add a click event for submit button

--- listeners.js
+++ listeners.js
@@ -6,6 +6,19 @@ document.addEventListener('click', function(e) {
     console.dir(e.target)
 });
 
+document.addEventListener('submit', function(e){
+    let target = e.target;
+    if (target.matches('#submit_review')) {
+        e.preventDefault();
+        let formData = {}
+        target.querySelectorAll('input').forEach(function(input){
+        formData[input.comment] = input.value;
+        })
+        debugger
+        Review.create(formData);
+    }
+})
+
 let element = document.getElementById("review-button")
 /* document.querySelectorAll(".review")
 returns multiple elements. Pick with [0] where 0 can be higher for multiple els */ 
