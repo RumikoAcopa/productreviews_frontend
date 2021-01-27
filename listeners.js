231be6d5a957f25a2161095c06021b420f8ccae0
rumikoacopa <rumikorios@gmail.com>
2021-01-27 08:34:11 -0500

    refactored DOMcontentloaded w/.then & added reset

--- listeners.js
+++ listeners.js
@@ -1,25 +1,26 @@
 document.addEventListener('DOMContentLoaded', function(e) {
-    Product.all();
-    Review.all();
-      
+    Product.all().then(() => Review.all());
 });
 
-document.addEventListener('click', function(e) {
-    console.dir(e.target)
-});
+const allProducts = [];
 
 document.addEventListener('submit', function(e){
     let target = e.target;
-    if(target.matches('#newItem')){
+    if(target.matches('.new-review-form')){
      e.preventDefault();
      let formData = {}
      target.querySelectorAll('input').forEach(function(input){
        formData[input.name] = input.value;
      })
-     Item.create(formData);
+     target.querySelectorAll('textarea').forEach(function(textarea){
+        formData[textarea.name] = textarea.value;
+      })
+     Review.create(formData);
      target.reset();
     }
 })
+
+
 
 
 
